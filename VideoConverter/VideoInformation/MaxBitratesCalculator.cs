1f7985b9d80d9713c5043d85410f29a1977a1410
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2023-11-02 14:05:16 +0100

    Adjusted max audio proportion to 15%

--- VideoConverter/VideoInformation/MaxBitratesCalculator.cs
+++ VideoConverter/VideoInformation/MaxBitratesCalculator.cs
@@ -2,14 +2,16 @@
 
 internal static class MaxBitratesCalculator
 {
+    private const double _maxAudioProportion = 0.15;
+
     public static (int AudioKilobitsPerSecond, int VideoKilobitsPerSecond) GetMaxBitrates(double maxMegabytes, TimeSpan duration)
     {
         var maxKilobits = maxMegabytes * 8000;
         var audioKilobits = duration.TotalSeconds * 160;
 
-        if (audioKilobits > maxKilobits * 0.05)
+        if (audioKilobits > maxKilobits * _maxAudioProportion)
         {
-            audioKilobits = maxKilobits * 0.05;
+            audioKilobits = maxKilobits * _maxAudioProportion;
         }
 
         var videoKilobits = maxKilobits - audioKilobits;
